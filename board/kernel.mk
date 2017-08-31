238757adc7de324348eb85d0add969399c4171ee
dimusa6 <dimusa.dms@gmail.com>
2017-08-31 16:40:16 +0200

    adapt common to piccolo

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,4 @@
-BOARD_DTBTOOL_ARGS := -2
-
+TARGET_KERNEL_ARCH := arm
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
@@ -9,7 +8,6 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 
 ENABLE_CPUSETS := true
 
-TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
-ifneq ($(FORCE_32_BIT),true)
-TARGET_USES_UNCOMPRESSED_KERNEL := true
-endif
+TARGET_KERNEL_SOURCE := kernel/bq/msm8916
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
